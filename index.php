eca340d9ab1828d7e0987f6bd8f95edf00d61360
Susha Anastasiia <sushaanastacia@yandex.ru>
2021-10-17 15:26:20 +0200

    movies.php: добавлен файл с массивом фильмов для отсортированного вывода в index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require "movies.php";
+
+echo "Hi! Enter your age: " . "\n";
+$userAge = readline("Age: ");
+
+if($userAge >= 0)
+	findMovie($userAge, $movies);
+else
+	echo "Please enter correct age.";
+
+function findMovie(int $userAge, array $movies): void{
+	$i = 1;
+	foreach ($movies as $film){
+		if($film['age_restriction'] <= $userAge){
+			echo "{$i}. {$film['title']} ({$film['release_year']}), {$film['age_restriction']}+. Rating: {$film['rating']};" . "\n";
+			$i ++;
+		}
+	}
+}
