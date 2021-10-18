0bf9027c97da1936df5f23c9f9412910a17e12b3
Susha Anastasiia <sushaanastacia@yandex.ru>
2021-10-18 19:24:23 +0200

    index.php: спрятаны все функции по обработке фильмов

--- index.php
+++ index.php
@@ -1,20 +1,15 @@
 <?php
+/** @var array $movies */
 require "movies.php";
 
 echo "Hi! Enter your age: " . "\n";
 $userAge = readline("Age: ");
 
-if($userAge >= 0)
-	findMovie($userAge, $movies);
+if(is_numeric($userAge))
+{
+	findMovie((int) $userAge, $movies);
+}
 else
-	echo "Please enter correct age";
-
-function findMovie(int $userAge, array $movies): void{
-	$i = 1;
-	foreach ($movies as $film){
-		if($film['age_restriction'] <= $userAge){
-			echo "{$i}. {$film['title']} ({$film['release_year']}), {$film['age_restriction']}+. Rating: {$film['rating']};" . "\n";
-			$i ++;
-		}
-	}
+{
+	echo "Invalid form! Please enter correct age.";
 }
