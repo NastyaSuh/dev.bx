4bfa18ecdeae8607be4a4f7384d8b7439ead3ef1
Susha Anastasiia <sushaanastacia@yandex.ru>
2021-11-30 18:22:49 +0200

    search via database

--- PHP/homework-7/index_detailed_movie.php
+++ PHP/homework-7/index_detailed_movie.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 /** @var array $movies */
 /** @var array $genres */
 /** @var array $config */
-require_once "data/movies.php";
+
 require_once "./lib/template_functions.php";
 require_once "./lib/helpers_functions.php";
 require_once "config/app.php";
